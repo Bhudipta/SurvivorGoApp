0e61ecbf330c2d5026d54669b055b15f089c5efd
Bhudipta <Bhudipta@users.noreply.github.com>
2018-02-14 03:51:34 +0600

    Client App for Survivorgo.com
    Client App for Survivorgo.com

---
+++ app/src/main/java/com/bhudipta/survivorgo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.bhudipta.survivorgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webb = (WebView)findViewById(R.id.shop);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("http://survivorgo.com");
+
+        mWebView = (WebView) findViewById(R.id.shop);
+        WebSettings webSettings= mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
